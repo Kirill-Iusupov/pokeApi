ee42dce77c6ae0d4b1b7734613de431f4a6fb352
Kirill-Iusupov <apriorifox.dev@gmail.com>
2022-09-27 03:51:58 +0600

    commit

--- src/components/pokemonInfo/pokemoninfo.jsx
+++ src/components/pokemonInfo/pokemoninfo.jsx
@@ -6,33 +6,19 @@ import { fetchPokemonInfo } from "../../redux/reducers/pokemonInfoSlice";
 
 
 const Pokemon = (props) =>{
-<<<<<<< HEAD
     const {pokemon} = useParams();
 
+
     const dispatch = useDispatch();
     const pokeInfo = useSelector(state => state.pokemonInfo)
-
-    console.log(pokeInfo.pokemonInfo.name);
 
     useEffect (()=>{
-=======
-    const {pokemon} = useParams()
-    const dispatch = useDispatch();
-    const pokeInfo = useSelector(state => state.pokemonInfo)
-    
-    useEffect(()=>{
->>>>>>> cba839a99bc141613769d623de5a05add01c5b7d
         dispatch(fetchPokemonInfo(pokemon))
     },[dispatch])
 
 
-<<<<<<< HEAD
 
 
-=======
-    console.log('Hello world!!!')
-    console.log(pokemon);
->>>>>>> cba839a99bc141613769d623de5a05add01c5b7d
     return (
         <div>
             <h1>This is {pokemon} page</h1>
